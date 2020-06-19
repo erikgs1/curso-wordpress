3e77b59811ffc2c748083bf84951f771c67d0105
erikgs1 <erik.contato15@gmail.com>
2020-06-19 16:45:23 -0300

    atualização do tema - aula 32

--- page-home.php
+++ page-home.php
@@ -42,19 +42,49 @@
                 <div class="row">
                     <?php get_sidebar('home'); ?>
                     <div class="news col-md-8">
-                        <?php 
-                            // Se houver algum post
-                            if(have_posts() ):
-                                // Enquanto houver posts, mostre-os para a gente
-                                while(have_posts()): the_post();
-                        ?>  
-                            
-                        <?php 
-                            endwhile;
-                        else:
-                        ?>
-                        <p> There's nothing yet to be displayed...</p>
-                        <?php endif; ?>
+                        <div class="container">
+                            <div class="row">
+                            <h1>Latest News</h1>
+                                <?php
+                                // Primeiro Loop 
+                                $featured = new WP_Query('post_type=post&posts_per_page=1&cat=4,3');
+                                if( $featured-> have_posts()):
+                                    while($featured->have_posts()): $featured->the_post();
+                                    ?>
+                                    <div class="col-12">
+                                        <?php get_template_part('template-parts/content', 'featured');
+                                        ?>
+                                    </div>
+                                    <?php
+                                    endwhile;
+                                    wp_reset_postdata();
+                                endif;
+                                ?>
+                                
+                                <?php
+                                //Segundo Loop
+                                $args = array(
+                                    'post_type' => 'post',
+                                    'posts_per_page' => 2,
+                                    'category__not_in' => array(5),
+                                    'category__in' => array(4,3),
+                                    'offset' => 1
+                                );
+                                $secondary = new WP_Query($args);
+                                if( $secondary-> have_posts()):
+                                    while($secondary->have_posts()): $secondary->the_post();
+                                    ?>
+                                    <div class="col-sm-6">
+                                        <?php get_template_part('template-parts/content', 'secondary');
+                                        ?>
+                                    </div>
+                                    <?php
+                                    endwhile;
+                                    wp_reset_postdata();
+                                endif;
+                                ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
